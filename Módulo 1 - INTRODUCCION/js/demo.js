5f021f02c0bd896a2fb7a8df04104398482cff63
Cristinash <cristina@astroprint.com>
2021-02-22 11:44:40 +0100

    Add files via upload

---
+++ Módulo 1 - INTRODUCCION/js/demo.js
@@ -0,0 +1,7 @@
+//El objetivo es mostrar por consola el contenido de uno de los input, pulsando un botón.
+
+document.getElementById("name").value = "My name";
+document.getElementById("lastName").value = "My last name";
+document.getElementById("avatar").src = "img/avatar.png";
+
+console.log("hola");
